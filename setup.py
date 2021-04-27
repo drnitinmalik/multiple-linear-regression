8488880a6acce8a7e58162aa05449ebbb3196442
Nitin Malik <73472150+drnitinmalik@users.noreply.github.com>
2021-04-27 22:43:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ from typing import Tuple, List
 from setuptools import Command, setup
 
 # Package meta-data
-name = "simple-linear-regression_python"
-description = "Simple Linear Regression in Python"
-url = "https://github.com/drnitinmalik/simple-linear-regression"
+name = "multiple-linear-regression_python"
+description = "Multiple Linear Regression in Python"
+url = "https://github.com/drnitinmalik/multiple-linear-regression"
 email = "nitinmalik77@gmail.com"
 author = "Nitin Malik"
 requires_python = ">=3.0.0"
